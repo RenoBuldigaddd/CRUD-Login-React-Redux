22ebf0f34351538f48f51c9ad15fc49238a81ac5
user <user@A24PC11>
2019-10-26 12:54:01 +0300

    creating entities

--- CRUD-Login-Beaver/ClientApp/src/components/Layout.js
+++ CRUD-Login-Beaver/ClientApp/src/components/Layout.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { Col, Grid, Row } from 'react-bootstrap';
+import { Container } from 'reactstrap';
 import NavMenu from './NavMenu';
 
 export default props => (
-  <Grid fluid>
-    <Row>
-      <Col sm={3}>
+    <div>
         <NavMenu />
-      </Col>
-      <Col sm={9}>
-        {props.children}
-      </Col>
-    </Row>
-  </Grid>
+        <Container>
+            {props.children}
+        </Container>
+    </div>
 );
+
--- CRUD-Login-Beaver/ClientApp/src/store/configureStore.js
+++ CRUD-Login-Beaver/ClientApp/src/store/configureStore.js
@@ -1,35 +1,42 @@
 ﻿import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import { routerReducer, routerMiddleware } from 'react-router-redux';
+import { connectRouter, routerMiddleware } from 'connected-react-router';
 import * as Counter from './Counter';
 import * as WeatherForecasts from './WeatherForecasts';
+import { createBrowserHistory } from 'history';
+
+
+// Create browser history to use in the Redux store
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
+export const history = createBrowserHistory({ basename: baseUrl });
+
 
 export default function configureStore(history, initialState) {
-  const reducers = {
-    counter: Counter.reducer,
-    weatherForecasts: WeatherForecasts.reducer
-  };
+    const reducers = {
+        counter: Counter.reducer,
+        weatherForecasts: WeatherForecasts.reducer
+    };
 
-  const middleware = [
-    thunk,
-    routerMiddleware(history)
-  ];
+    const middleware = [
+        thunk,
+        routerMiddleware(history)
+    ];
 
-  // In development, use the browser's Redux dev tools extension if installed
-  const enhancers = [];
-  const isDevelopment = process.env.NODE_ENV === 'development';
-  if (isDevelopment && typeof window !== 'undefined' && window.devToolsExtension) {
-    enhancers.push(window.devToolsExtension());
-  }
+    // In development, use the browser's Redux dev tools extension if installed
+    const enhancers = [];
+    const isDevelopment = process.env.NODE_ENV === 'development';
+    if (isDevelopment && typeof window !== 'undefined' && window.devToolsExtension) {
+        enhancers.push(window.devToolsExtension());
+    }
 
-  const rootReducer = combineReducers({
-    ...reducers,
-    routing: routerReducer
-  });
+    const rootReducer = combineReducers({
+        ...reducers,
+        router: connectRouter(history)
+    });
 
-  return createStore(
-    rootReducer,
-    initialState,
-    compose(applyMiddleware(...middleware), ...enhancers)
-  );
+    return createStore(
+        rootReducer,
+        initialState,
+        compose(applyMiddleware(...middleware), ...enhancers)
+    );
 }
